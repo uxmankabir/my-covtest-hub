7e679034c281b3abd1746498833f2ac5ae0dd386
Usman Kabir <usman.kabir.k@gmail.com>
2021-03-11 16:29:31 +0500

    pg setup and seed file added

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_15_000121) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(username: 'admin', password: '12345')
+Admin.create(username: 'tester', password: 'abcde')
+
+HomeTestKit.create(ttn_code: 'MM2874Z6')
+HomeTestKit.create(ttn_code: 'FEQQ6UUG')
+HomeTestKit.create(ttn_code: '34GC829B')
+HomeTestKit.create(ttn_code: 'CB8FBCCM')
+HomeTestKit.create(ttn_code: '8RL4ENTK')
+HomeTestKit.create(ttn_code: '57UBS5J6')
+HomeTestKit.create(ttn_code: '4F7YKH9G')
+HomeTestKit.create(ttn_code: 'R9KZ2NXL')
+HomeTestKit.create(ttn_code: 'YBQUVXHL')
+HomeTestKit.create(ttn_code: 'CCZTQBKW')
